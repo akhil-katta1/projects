cc39d75bfbf92b99e27383024ffcadd2df00a17e
akhil-katta1 <115378946+akhil-katta1@users.noreply.github.com>
2025-04-11 17:30:59 -0500

    Update README.py

--- README.py
+++ README.py
@@ -1,6 +1,2 @@
-# Get unique job codes and convert all to strings
-job_codes = dup['Job Code'].dropna().unique().tolist()
-job_codes = [str(code) for code in job_codes]
-
-# Print as comma-separated string
-print(", ".join(job_codes))
+df_pa.drop_duplicates(inplace=True)
+df_pa.reset_index(drop=True, inplace=True)
