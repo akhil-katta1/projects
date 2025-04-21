92d35777f83add2c4ce9fefe7f4b777bd5c5b2ed
akhil-katta1 <115378946+akhil-katta1@users.noreply.github.com>
2025-04-21 16:31:30 -0500

    Update README.py

--- README.py
+++ README.py
@@ -1,11 +1,1 @@
-# Set pandas to display all rows and columns
-pd.set_option('display.max_rows', None)  # No limit to the number of rows
-pd.set_option('display.max_columns', None)  # No limit to the number of columns
-pd.set_option('display.width', None)  # To avoid line wrapping
-pd.set_option('display.max_colwidth', None)  # Show full column content
-
-# Find duplicate rows (including the first occurrence)
-duplicates = df[df.duplicated(keep=False)]  # This will include all duplicates
-
-# Print the entire table of duplicates
-print(duplicates)
+michigan_ingest_df['Converted_to_Annual_flag'] = michigan_ingest_df['10/01/2023 Minimum'] < 1000
