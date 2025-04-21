16fd9a089952208822718cbb052715ce3dd2acbb
akhil-katta1 <115378946+akhil-katta1@users.noreply.github.com>
2025-04-21 17:47:14 -0500

    Update README.py

--- README.py
+++ README.py
@@ -1,1 +1,17 @@
-michigan_ingest_df['Converted_to_Annual_flag'] = michigan_ingest_df['10/01/2023 Minimum'] < 1000
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType, FloatType
+
+# Define the schema
+schema = StructType([
+    StructField("Job Code", StringType(), True),
+    StructField("Job Title", StringType(), True),
+    StructField("Pay Scale Group", FloatType(), True),
+    StructField("Pay Scale Type", StringType(), True),
+    StructField("Bargaining Unit", StringType(), True),
+    StructField("Salary Range", StringType(), True),
+    StructField("Min Salary", FloatType(), True),
+    StructField("Max Salary", FloatType(), True),
+    StructField("Mean Salary", FloatType(), True)
+])
+
+# Now you can create a Spark DataFrame with the schema
+df = spark.createDataFrame(df_pandas, schema=schema)
